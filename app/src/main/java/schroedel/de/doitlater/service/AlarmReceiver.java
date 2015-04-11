caff2ef7f70be3c598a0022a3f7e9f34b848380f
Christian Schrödel <schroedel.christian@googlemail.com>
2015-04-11 20:37:26 +0200

    Added category and priority to nofication

--- app/src/main/java/schroedel/de/doitlater/service/AlarmReceiver.java
+++ app/src/main/java/schroedel/de/doitlater/service/AlarmReceiver.java
@@ -1,11 +1,11 @@
 package schroedel.de.doitlater.service;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
 
 import schroedel.de.doitlater.R;
 import schroedel.de.doitlater.content.ToDoDatabase;
@@ -24,8 +24,6 @@ public class AlarmReceiver extends BroadcastReceiver
 		ToDoItem item;
 		long id = -1;
 
-		Log.d("AlarmReceiver", "Alarm received");
-
 		if (data.hasExtra(ToDoItem.EXTRA_ID))
 			id = data.getLongExtra(ToDoItem.EXTRA_ID, -1);
 
@@ -39,6 +37,8 @@ public class AlarmReceiver extends BroadcastReceiver
 		builder.setSmallIcon(R.drawable.ic_launcher);
 		builder.setContentTitle(item.title);
 		builder.setContentText(item.description);
+		builder.setCategory(Notification.CATEGORY_ALARM);
+		builder.setPriority(Notification.PRIORITY_HIGH);
 
 		NotificationManager manager = (NotificationManager)
 			context.getSystemService(Context.NOTIFICATION_SERVICE);
