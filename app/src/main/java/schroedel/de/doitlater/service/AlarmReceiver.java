39c154ad28fd4213806af905d7ce6deb33acb35c
Christian Schrödel <schroedel.christian@googlemail.com>
2015-04-12 12:10:54 +0200

    Added actions to Notification and additional receiver to update items

--- app/src/main/java/schroedel/de/doitlater/service/AlarmReceiver.java
+++ app/src/main/java/schroedel/de/doitlater/service/AlarmReceiver.java
@@ -40,12 +40,26 @@ public class AlarmReceiver extends BroadcastReceiver
 		Intent detailIntent = new Intent(context, ItemListActivity.class);
 		detailIntent.putExtra(ToDoItem.EXTRA_ID, id);
 
+		Intent laterIntent = new Intent(context, UpdateItemReceiver.class);
+		laterIntent.putExtra(ToDoItem.EXTRA_ID, id);
+		laterIntent.setAction(UpdateItemReceiver.ACTION_LATER);
+
+		Intent doneIntent = new Intent(context, UpdateItemReceiver.class);
+		doneIntent.putExtra(ToDoItem.EXTRA_ID, id);
+		doneIntent.setAction(UpdateItemReceiver.ACTION_DONE);
+
 		TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
 		stackBuilder.addParentStack(ItemListActivity.class);
 		stackBuilder.addNextIntent(detailIntent);
 
-		PendingIntent pendingIntent =
+		PendingIntent content =
 			stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+		PendingIntent later =
+			PendingIntent.getBroadcast(context, 0, laterIntent, 0);
+
+		PendingIntent done =
+			PendingIntent.getBroadcast(context, 0, doneIntent, 0);
 
 		NotificationCompat.Builder builder =
 			new NotificationCompat.Builder(context);
@@ -54,14 +68,17 @@ public class AlarmReceiver extends BroadcastReceiver
 		builder.setContentText(item.description);
 		builder.setCategory(Notification.CATEGORY_ALARM);
 		builder.setPriority(Notification.PRIORITY_HIGH);
-		builder.setContentIntent(pendingIntent);
+		builder.setContentIntent(content);
+		builder.addAction(R.drawable.ic_launcher, "later", later);
+		builder.addAction(R.drawable.ic_launcher, "done", done);
 
-		Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+		Uri alarmSound = RingtoneManager.getDefaultUri(
+			RingtoneManager.TYPE_ALARM);
 		builder.setSound(alarmSound);
 		builder.setVibrate(new long[] { 1000, 500, 1000, 500 });
 
 		NotificationManager manager = (NotificationManager)
 			context.getSystemService(Context.NOTIFICATION_SERVICE);
-		manager.notify(1, builder.build());
+		manager.notify((int) id, builder.build());
 	}
 }
---
+++ app/src/main/java/schroedel/de/doitlater/service/UpdateItemReceiver.java
@@ -0,0 +1,87 @@
+package schroedel.de.doitlater.service;
+
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import schroedel.de.doitlater.content.ToDoDatabase;
+import schroedel.de.doitlater.content.ToDoItem;
+
+/**
+ * Created by Christian Schrödel on 11.04.15.
+ *
+ * Receiver receiving intents to update to do list items.
+ */
+public class UpdateItemReceiver extends BroadcastReceiver
+{
+	public static final String ACTION_LATER = "later";
+	public static final String ACTION_DONE = "done";
+
+	@Override
+	public void onReceive(Context context, Intent intent)
+	{
+		String action = intent.getAction();
+
+		if (action.equalsIgnoreCase(ACTION_DONE))
+			itemIsDone(context, intent);
+		else if (action.equalsIgnoreCase(ACTION_LATER))
+			doItemLater(context, intent);
+	}
+
+	/**
+	 * Marks to do list item as done.
+	 *
+	 * @param context - context
+	 * @param doneIntent - intent containing data of to do list item
+	 */
+	private void itemIsDone(Context context, Intent doneIntent)
+	{
+		long id = -1;
+
+		if (doneIntent.hasExtra(ToDoItem.EXTRA_ID))
+			id = doneIntent.getLongExtra(ToDoItem.EXTRA_ID, -1);
+
+		if (id == -1)
+			return;
+
+		ToDoDatabase.getInstance(context).updateItemIsDone(
+			id,
+			ToDoItem.ITEM_DONE);
+
+		cancelNotification(context, id);
+	}
+
+	/**
+	 * Sets alarm of to do list items to a later date.
+	 *
+	 * @param context - context
+	 * @param laterIntent - intent containing data of to do list item
+	 */
+	private void doItemLater(Context context, Intent laterIntent)
+	{
+		long id = -1;
+
+		if (laterIntent.hasExtra(ToDoItem.EXTRA_ID))
+			id = laterIntent.getLongExtra(ToDoItem.EXTRA_ID, -1);
+
+		if (id == -1)
+			return;
+
+		cancelNotification(context, id);
+	}
+
+	/**
+	 * Cancels existing notification matching giving id.
+	 *
+	 * @param context - context
+	 * @param id - id of notification
+	 */
+	private void cancelNotification(Context context, long id)
+	{
+		NotificationManager manager = (NotificationManager)
+			context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+		manager.cancel((int) id);
+	}
+}
